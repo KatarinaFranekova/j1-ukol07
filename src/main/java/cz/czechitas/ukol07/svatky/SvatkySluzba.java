4115be7e1e1d2cd38388a15f3412e681060b471f
KatarinaFranekova <katarinafranekova22@gmail.com>
2023-05-02 22:24:45 +0200

    ukol 7 uprava podla pokynov

--- src/main/java/cz/czechitas/ukol07/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol07/svatky/SvatkySluzba.java
@@ -19,7 +19,7 @@ public class SvatkySluzba {
     private final SeznamSvatku seznamSvatku;
 
     public SvatkySluzba() throws IOException {
-        // TODO načíst seznam svátků ze souboru svatky.json
+
         seznamSvatku = objectMapper.readValue(cestaKDatum.toFile(), SeznamSvatku.class);
 
     }
@@ -35,17 +35,5 @@ public class SvatkySluzba {
                 .map(svatek -> svatek.getJmeno())
                 .toList();
 
-
-
-        // TODO
-        // získat seznam svátků
-        // převést na Stream
-        // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
-        // pomocí metody map() získat z objektu jméno
-        // pomocí toList() převést na List
-
-
-
-        // Následující řádek po vlastní implementaci smažete.
     }
 }
